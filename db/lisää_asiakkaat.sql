d1b30b3a12d525d7a4dd98423c267b93303dda33
Eetu <eetu.urhemaa@gmail.com>
2018-05-01 21:32:07 +0300

    Lisätty tietokanta scriptit

---
+++ db/lisää_asiakkaat.sql
@@ -0,0 +1,7 @@
+insert into Asiakas(etunimi, sukunimi, lahiosoite, postitoimipaikka)
+	values('Eetu', 'Jokinen', 'Peltokatu 4', 'Kuopio'),
+	('Tommi', 'Tomminen', 'Asemakatu 1', 'Kuopio'),
+	('Jukka', 'Juntunen', 'Ratakatu 1', 'Kuopio'),
+	('Liisa', 'Viitanen', 'Junakatu 31', 'Kuopio'),
+	('Maija', 'Virtanen', 'Vuorikatu 2', 'Kuopio'),
+	('Kalle', 'Vuori', 'Retkeilijäntie 7', 'Kuopio');
